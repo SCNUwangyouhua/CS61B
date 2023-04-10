f57ae512145a36fde9a37df884f6102c60ab7f3a
wangyouhua <20222131060@m.scnu.edu.cn>
2023-04-10 17:06:15 +0800

    lab3's works

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -4,9 +4,49 @@ import edu.princeton.cs.algs4.StdRandom;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+
+
 /**
  * Created by hug.
  */
 public class TestBuggyAList {
+
   // YOUR TESTS HERE
+    public static void randomizedTest(){
+        AListNoResizing<Integer> L = new AListNoResizing<>();
+        BuggyAList<Integer> BL = new BuggyAList<>();
+
+        int N = 5000;
+        for (int i = 0; i < N; i += 1) {
+            int operationNumber = StdRandom.uniform(0, 4);
+            if (operationNumber == 0) {
+                // addLast
+                int randVal = StdRandom.uniform(0, 100);
+                L.addLast(randVal);
+                BL.addLast(randVal);
+                System.out.println("addLast(" + randVal + ")");
+            } else if (operationNumber == 1) {
+                // size
+                int size = L.size();
+                int sizeBL = BL.size();
+                System.out.println("L.size: " + size + " BL.size = " + sizeBL);
+                assertEquals(size, sizeBL);
+            } else if (operationNumber == 2 && L.size() > 0 && BL.size() > 0){//getLast
+                int LastVal = L.getLast();
+                int BLastVal = BL.getLast();
+                System.out.println("L lastVal = " + LastVal + " BL lastVal = " + BLastVal);
+                assertEquals(LastVal, BLastVal);
+            } else if (operationNumber == 3 && L.size() > 0 && BL.size() > 0){//removeLast
+                int ret = L.removeLast();
+                int ret2 = BL.removeLast();
+                System.out.println("called to removeLast");
+                assertEquals(ret, ret2);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        randomizedTest();
+    }
+
 }
--- lab3/timingtest/TimeSLList.java
+++ lab3/timingtest/TimeSLList.java
@@ -1,5 +1,7 @@
 package timingtest;
+import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.Stopwatch;
+import org.checkerframework.checker.units.qual.A;
 
 /**
  * Created by hug.
@@ -23,6 +25,30 @@ public class TimeSLList {
 
     public static void timeGetLast() {
         // TODO: YOUR CODE HERE
+        //System.out.println(11451);
+        SLList<Integer> testSLList = new SLList<>();
+        AList<Integer> Ns = new AList<>();
+        AList<Double> times = new AList<>();
+        AList<Integer> opCounts = new AList<>();
+        for (int i = 1000; i <= 128000; i *= 2){
+            //System.out.println("no at i = " + i);
+            Ns.addLast(i);
+            int opCount = 0;
+            for (int j = 0; j < i; ++j){
+                testSLList.addLast(j);
+            }
+            double startTime = System.currentTimeMillis();
+            for (int k = 1; k <= 10000; ++k){
+                testSLList.getLast();
+                opCount += 1;
+            }
+            double endTime = System.currentTimeMillis();
+            double time = (endTime - startTime) / 1000;
+            opCounts.addLast(opCount);
+            times.addLast(time);
+        }
+        //System.out.println(114514);
+        printTimingTable(Ns, times, opCounts);
     }
 
 }
